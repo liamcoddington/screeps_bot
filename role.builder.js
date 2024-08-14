5979a341312b3fb75df7157b125ce17532554f4b
liamcoddington <liamcoddington@proton.me>
2024-08-13 22:43:18 -0500

    Added construction site selection

--- role.builder.js
+++ role.builder.js
@@ -4,10 +4,10 @@ let upgraderFunctions = require("role.upgrader.functions");
 let builder = {
   /** @param {Creep} creep **/
   run: function (creep) {
-    if (creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+    if (creep.memory.building && creep.store[RESOURCE_ENERGY] === 0) {
       creep.memory.building = false;
       creep.say("🔄 harvest");
-    } else if (!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+    } else if (!creep.memory.building && creep.store.getFreeCapacity() === 0) {
       creep.memory.building = true;
       creep.say("🚧 build");
     }
@@ -15,13 +15,52 @@ let builder = {
     if (creep.memory.building) {
       let targets = creep.room.find(FIND_CONSTRUCTION_SITES);
       if (targets.length > 0) {
-        if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+        if (creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
           creep.moveTo(targets[0], {
             visualizePathStyle: { stroke: "#ffffff" },
           });
         }
       } else {
-        upgraderFunctions.upgrade(creep);
+        let extensionStructures = creep.room.find(FIND_STRUCTURES, {
+          filter: (structure) => {
+            return structure.structureType === STRUCTURE_EXTENSION;
+          },
+        });
+        if (extensionStructures.length < 5) {
+          let spawnPos = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+              return structure.structureType === STRUCTURE_SPAWN;
+            },
+          })[0].pos;
+
+          // Arbitrary for now
+          let possiblePositions = [
+            new RoomPosition(spawnPos.x + 2, spawnPos.y, spawnPos.roomName),
+            new RoomPosition(spawnPos.x + 2, spawnPos.y + 1, spawnPos.roomName),
+            new RoomPosition(spawnPos.x - 2, spawnPos.y, spawnPos.roomName),
+            new RoomPosition(spawnPos.x - 2, spawnPos.y + 1, spawnPos.roomName),
+            new RoomPosition(spawnPos.x, spawnPos.y + 2, spawnPos.roomName),
+          ];
+
+          let extensionPos;
+          for (let i in possiblePositions) {
+            let pos = possiblePositions[i];
+            let isUsed = false;
+            for (let j in extensionStructures) {
+              let extensionStructure = extensionStructures[j];
+              if (pos.isEqualTo(extensionStructure.pos)) {
+                isUsed = true;
+              }
+            }
+            if (!isUsed) {
+              extensionPos = pos;
+            }
+          }
+          console.log("creating extension at ", extensionPos);
+          creep.room.createConstructionSite(extensionPos, STRUCTURE_EXTENSION);
+        } else {
+          upgraderFunctions.upgrade(creep);
+        }
       }
     } else {
       harvesterFunctions.harvest(creep);
